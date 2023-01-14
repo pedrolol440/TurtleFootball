16a8e780d2b28c139b3d32f18416afc2eca77764
Pedro Baeza <83214961+pedrolol440@users.noreply.github.com>
2023-01-14 20:14:31 +0100

    Con comentarios

--- TurtleFootball.py
+++ TurtleFootball.py
@@ -19,7 +19,8 @@ import math
 
 class Nodo(object):
     def __init__(self):
-        # Params
+
+        # Variable de la clase
         self.r = rospy.Rate(1)
         self.image = None
         self.br = CvBridge()
@@ -35,35 +36,37 @@ class Nodo(object):
         self.first_odom_check = False
         self.estado=0
         self.cX_g=0
+        
         # Node cycle rate (in Hz).
         self.loop_rate = rospy.Rate(1)
         self.image_rgb = None
+        
         # Publishers
        
-        self.pub = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=5)
-        self.pub2 = rospy.Publisher("/cmd_vel", Twist, queue_size=5)
-        self.pub3 = rospy.Publisher("/mobile_base/commands/reset_odometry", Empty, queue_size=1)
+        self.pub = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=5) #Publica movimientos en el robot real
+        self.pub2 = rospy.Publisher("/cmd_vel", Twist, queue_size=5)  #Publica movimientos en el robot simulado
+        self.pub3 = rospy.Publisher("/mobile_base/commands/reset_odometry", Empty, queue_size=1) #Resetea la odometría
 
         # Subscribers
         self.sub = rospy.Subscriber(
-            "/camera/depth/image_raw", Image, self.callback)
-        self.sub2 = rospy.Subscriber("/camera/rgb/image_raw",Image,self.callback2)
+            "/camera/depth/image_raw", Image, self.callback) #Para obetener el frame de la camara de profundidad
+        self.sub2 = rospy.Subscriber("/camera/rgb/image_raw",Image,self.callback2) #Para obetener el frame de la camara de color
         
-        self.sub3 = rospy.Subscriber('/odom', Odometry, self.get_rotation)
+        self.sub3 = rospy.Subscriber('/odom', Odometry, self.get_rotation) #Para obtener la odometría del robot
         
 
     
-
+    #Función callback que obtiene la imagen de profundidad
     def callback(self, msg):
         # rospy.loginfo('Image received...')
         self.image = self.br.imgmsg_to_cv2(msg)
  
-
+    #Función callback que obtiene la imagen de color
     def callback2(self, msg):
         # rospy.loginfo('Image received...')
         self.image_rgb = self.br.imgmsg_to_cv2(msg, desired_encoding='bgr8')
         
-
+    #Función callback que obtiene las orientaciones del robot de la odometria y las pasa a grados euler
     def get_rotation(self,msg):
 
         orientation_q = msg.pose.pose.orientation
@@ -73,21 +76,28 @@ class Nodo(object):
       
 
 
-
-    
+    #Función que hace rotar al robot +90 grados
     def do_rotation(self):
         
+        #Reseteamos la odometria
         self.pub3.publish(Empty())
-        grados = math.degrees(self.yaw)
 
-        # print("LOS DE LA FUNCION ",grados)
-        g_g = 90
-        # g_g = math.degrees(self.grados_goal)
+        #Conversión a grados sexagesimales
+        grados = math.degrees(self.yaw)
         
+        #Se define los grados que se quiere girar
+        g_g = 90
+        
+        #Conversión en caso de obtener grados negativos de la odometría
         if grados < 0:
             grados = grados + 360
         
-        rotacion_terminada = False
+
+        rotacion_terminada = False #condición de terminación del bucle de giro
+
+        #BUCLE DE GIRO: se actualiza el valor de los grados actuales en cada iteracion,
+        # se restan los grados desesados a los actuales y se mantiene una velocidad de rotación
+        # mientras la resta sea positiva. Al finalizar se detiene el robot. 
         while rotacion_terminada == False:
 
             grados = math.degrees(self.yaw)
@@ -103,8 +113,13 @@ class Nodo(object):
                 rotacion_terminada = True
                 self.cmd.angular.z = 0            
                 self.pub.publish(self.cmd)
-        self.pub3.publish(Empty())
+        self.pub3.publish(Empty()) #Volvemos a resetear la odometria
 
+
+    #Función que hace rotar al robot -90 grados
+    
+    # El funcionamiento es el mismo que el movimiento de +90 grados, pero indicando como target -90 y 
+    # en la resta relizando el valor absoluto para evitar problemas en la lógica.
     def do_negative_rotation(self):
 
         #print("hola")        
@@ -135,6 +150,13 @@ class Nodo(object):
                 self.pub.publish(self.cmd)       
         self.pub3.publish(Empty())         
 
+
+    #Función donde se implementa el movimiento hacia la izquierda del robot:
+
+    #-Se implementa una sencilla máquina de estados que llama primero a la rotación de +90º,
+    # posteriormente aplica una velocidad lineal durante un tiempo limitado y finalmente realiza una rotación
+    # de -90º para volver a la orientación inicial. Entre estados, se introducen pequeños bucles vacios que funcionan a modo
+    # de ligeros retardos para evitar que los movimientos se superpongan.
     def movimiento(self):
         state = 0
 
@@ -178,6 +200,9 @@ class Nodo(object):
             while c<20000000:    
                 c= c+1            
 
+
+    #Función que implementa el movimiento hacia la derecha del robot, identica a la anterior, pero con 
+    # la diferencia de llamar primero a la rotación de -90º y posterioremente a la rotación de +90º.
     def movimiento2(self):
         state = 0
         self.pub3.publish(Empty())         
@@ -224,55 +249,42 @@ class Nodo(object):
     
 
 
-
+    ##FUNCION PRINCIPAL ##
+    #En esta se encuentra la implementación del programa.
     def start(self):
             
         
-        # self.pub3.publish(Empty())
+        # Ligero retardo que permite que se inicialicen los valores de la odometría
         c=0
         while c<20000000:    
-            c= c+1
-            
+            c= c+1            
         if self.first_odom_check:  
             c = 0
-            
-       
-        # self.movimiento2()    
-        
-        # self.movimiento2()
-        # self.movimiento2()   
-            
-        while True:
-            #if self.image is not None and self.vagabundeo.data==False:
-            # frame = "a"
 
+        #Bucle principal del programa                
+        while True:
+            
+            
+            ## Primero comprobamos que haya imagen y que nos encontramos en un estado de los disponibles ##
+                         
             if (self.image_rgb is not None) and (self.image is not None) and self.estado<5 :
                 frame = self.image_rgb
                 
-                # if frame=="a":
-                #     skip
+                
+                
                 hight,width,_ = frame.shape
-                # Conversión de espacio RGB a HSV
-            
+                
+                # Conversión de espacio RGB a HSV            
                 HSV = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-                x,y,w,h=0,0,0,0
-                # # 2- define the range of red
-                # lower=np.array([100, 0, 0])
-                # upper=np.array([255, 255,255])
-
-                # #check if the HSV of the frame is lower or upper red
-                # Red_mask = cv2.inRange(HSV,lower, upper)
-                # result = cv2.bitwise_and(frame, frame, mask = Red_mask)
-                    #Rango y máscara para el color rojo
-
+                x,y,w,h=0,0,0,0                        
+                
+                #Rango y máscara para el color rojo
                 red_lower = np.array([136, 87, 111], np.uint8)
-                red_upper = np.array([180, 255, 255], np.uint8)
-                # red_lower = np.array([250, 82, 84], np.uint8)
-                # red_upper = np.array([7, 102, 104], np.uint8)                
+                red_upper = np.array([180, 255, 255], np.uint8)                            
                 red_mask = cv2.inRange(HSV, red_lower, red_upper)
-                #print(red_mask)
+                
 
-                                    #Rango y máscara para el color verde
+                #Rango y máscara para el colora marillo (a pesar de poner green)
                 green_lower = np.array([20, 100, 100], np.uint8)
                 green_upper = np.array([30, 255, 255], np.uint8)
                 green_mask = cv2.inRange(HSV, green_lower, green_upper)
@@ -281,39 +293,59 @@ class Nodo(object):
                 #Definimos kernel para la operación morfológica
                 kernal = np.ones((5, 5), "uint8")
 
-                # For red color
+                #Operaciones morfológicas para definir mejor los contornos:
+
+                    # Para el color rojo
                 red_mask = cv2.dilate(red_mask, kernal)
                 res_red = cv2.bitwise_and(frame, frame,
                                         mask=red_mask)
 
-                # For green color
+                    # Para el color amarillo
                 green_mask = cv2.dilate(green_mask, kernal)
                 res_green = cv2.bitwise_and(frame, frame,
                                             mask=green_mask)                    
 
-                # Draw rectangular bounded line on the detected red area
+                # Obtenemos los contornos detectados como rojos
                 contours, hierarchy = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
                 
-                # Draw rectangular bounded line on the detected red area
+                # Obtenemos los contornos detectados como amarillos
                 contours_g, hierarchy = cv2.findContours(green_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+                #Si hemos encontrado contornos:
                 if len(contours) != 0 and len(contours_g):
 
-                                    # Find the index of the largest contour
+                    # Obtenemos las areas de los contornos rojos
                     areas = [cv2.contourArea(c) for c in contours]
+                    
                     if areas != 0:
+
+                        #Sacamos el area de mayor tamaño, que corresponde con el jugador
                         max_index = np.argmax(areas)
                         red_cnt=contours[max_index]
+
+                        #Sacamos las coordenadas de su bounding box
                         x_r,y_r,w_r,h_r = cv2.boundingRect(red_cnt)
+
+                        #Dibujamos la bounding box
                         frame = cv2.rectangle(frame, (x_r, y_r), (x_r+w_r, y_r+h_r), (0, 0, 255), 2)
+                        
+                        #Sacamos las coordenadas del centroide del jugador a partir de las coordenadas de la bounding box
+                        # También debemos asegurarnos de no superar las dimensiones de la imagen
+                        
+                        #Coordenada X del centroide:
                         cX = int((x_r+x_r+w_r)/2)
                         if cX >480:
                             cX = 479
+                        
+                        #Coordenada Y del centroide:    
                         cY = int((y_r+y_r+h_r)/2)
                         if cY >480:
-                            cY = 479                          
+                            cY = 479
+
+                        #Finalmente, se dibuja el centroide en la imagen 
                         cv2.circle(frame, (cX,cY), 5, (255,255,255), -1)
 
+                    #Repetimos el proceso de deteccion del centroide de la pelota, analizando los contornos amarillos ahora:
                     areas_g = [cv2.contourArea(d) for d in contours_g]
                     if areas_g != 0:
                         max_index_g = np.argmax(areas_g)
@@ -337,95 +369,119 @@ class Nodo(object):
                     
                     
 
-                    # TODO: Anadir la funcion que lea la profundidad de los objetos en camara
+                    #Definición del estado actual (si nos encontramos en el estado 0,1 o 2)
+
+                    #Si se han detectado los centroides
                     if areas_g != 0 and areas != 0:
-                        distancia_media = self.image[cX][cY]
-                        distancia_verde = self.image[self.cX_g][cY_g]
-                        #print("distancia rojo: " + str(distancia_media))
-                        #print("distancia amarillo: " + str(distancia_verde))    
-                                
-
-                        # TODO: Movernos hasta un punto cercano (a decidir) a la pelota 
-
                         
+                        #Diferencia entre las coordenadas X de los centroides            
                         dx = self.cX_g - cX
                         print(dx)
+
+                        #Umbral de alineación mínima 
                         threshold1 = 15
                         
-                        #     if dx > 0:                                 
+                        #Si la diferencia entre los centroides es mayor que el umbral, es decir, no se ha alineado aun                             
                         if abs(dx) > threshold1:
                             
-
-                            
+                            #Si la pelota esta a la derecha del jugador
                             if dx > 0 and self.estado !=3 and self.estado!=2:
-                                
                                 self.estado=1
-                            if dx < 0 and self.estado !=3 and self.estado!=1:#si esta a la izq
-                                
+
+                            #Si la pelota esta a la izquierda del jugador
+                            if dx < 0 and self.estado !=3 and self.estado!=1:
                                 self.estado=2
-                                
+
+                        #Si la diferencia entre los centroides es menor que el umbral, es decir, ya se han alineado
                         if abs(dx) < threshold1 :
-                            if self.estado==1:#dcha
+
+                            #Si se encontraba la pelota a la derecha del jugador, se pasa al estado 3
+                            if self.estado==1:
                                 self.estado = 3 
 
-                            if self.estado==2:#izq
+                            #Si se encontraba la pelota a la izquierda del jugador, se pasa al estado 3
+                            if self.estado==2:
                                 self.estado = 4
                             
                             
+            #Acciones a realizar según el estado
 
-            if self.estado == 1: #si esta a la dcha
-                
+            #Estado 1 (la pelota está a la derecha del jugador y los centroides no estan alineados)
+            if self.estado == 1:
                 
                 print("estado 1")
+
+                #Se realiza un movimiento a la derecha para encontrar una nueva perspectiva
                 self.movimiento2()
 
-
-            if self.estado ==2: #si esta a la izq
+            #Estado 2 (la pelota está a la izquierda del jugador y los centroides no estan alineados)
+            if self.estado ==2: #
                 
-                
+                #Se realiza un movimiento a izquierda para encontrar una nueva perspectiva
                 print("estado 2")
                 self.movimiento()
-                          
-            if self.estado == 3:#dcha
+
+            #Estado 3  el robot ya esta en la diagonal y tiene que orientarse hacia la pelota            
+            if self.estado == 3:
+
                 print("estado 3")
+
+                # Se define otro umbral de alineación minima
                 threshold2=10
                 
-                print(self.cX_g-240)   
+                #Se obtiene difX y se compar con el umbral
+
+                #Si el centroide de la pelota y el centro de la imagen no están alineados    
                 if abs(self.cX_g - 240)  >= threshold2:
                     
+                    #El robot comienza a girar sobre si mismo a la izquierda
                     self.cmd.angular.z = 0.3
                     self.pub.publish(self.cmd)
 
+                #Si el centroide de la pelota y el centro de la imagen están alineados
                 if abs(self.cX_g - 240)   < threshold2:
                     
+                    #El robot para de girar y pasa al ultimo estado
                     self.cmd.angular.z = 0
                     self.pub.publish(self.cmd)
                     self.estado=5  
 
+            #Estado 4  el robot ya esta en la diagonal y tiene que orientarse hacia la pelota 
+            if self.estado == 4:
 
-            if self.estado == 4:#izq
                 print("estado 4")
+
+                # Se define otro umbral de alineación minima
                 threshold2=10
                 
-                print(self.cX_g-240)   
+                #Se obtiene difX y se compar con el umbral
+
+                 #Si el centroide de la pelota y el centro de la imagen no están alineados 
                 if abs(self.cX_g - 240)  >= threshold2:
                     
+                    #El robot comienza a girar sobre si mismo a la derecha
                     self.cmd.angular.z = -0.3
                     self.pub.publish(self.cmd)
 
                 if abs(self.cX_g - 240)   < threshold2:
                     
+                    #El robot para de girar y pasa al ultimo estado
                     self.cmd.angular.z = 0
                     self.pub.publish(self.cmd)
                     self.estado=5  
 
+            #Estado 5 el robot ya esta correctamente posicionado y orientado para realizar el pase
             if self.estado==5:
+
                 print("estado 5")
+
+                #Avanza en linea recta durante un tiempo determinado por la variable c en el while(determinado experimentalmente)
                 c=0
                 while c < 60000:
                     self.cmd.linear.x = 1
                     self.pub.publish(self.cmd)
                     c=c+1
+                #una vez se ha consumido este tiempo y se ha golpeado la pelota, se para el robot y acaba el programa
                 self.cmd.linear.x = 0
                 self.pub.publish(self.cmd) 
                 break                   
@@ -437,12 +493,10 @@ class Nodo(object):
 
     
 
-
+#Función main donde se lanza el nodo y el programa principal de este
 if __name__ == '__main__':
 
     rospy.init_node("tracking", anonymous=True)
     my_node = Nodo()
     my_node.start()
-    # while not rospy.is_shutdown():
-        # my_node.r.sleep()
-    #my_node.pase_al_rojo()
+    
